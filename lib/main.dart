08f8df94fa4ef62e0de37cbbdb5a59af76e2ba06
Sean Alexander Porras <spporras@up.edu.ph>
2024-05-25 04:35:45 +0800

    Initializing Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async{
+  WidgetsFlutterBinding
+      .ensureInitialized(); // Ensure flutter bindings are initialized
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+
   runApp(const MyApp());
 }
 
