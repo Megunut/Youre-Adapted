cca13f0396b563f1b2ee17b6d4915f54fcf030f3
rainiken27 <motrifalgar@up.edu.ph>
2024-05-30 18:23:57 +0800

    blank profile page, placeholder

---
+++ lib/profile.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
