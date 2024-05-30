8f40b92d4990a6e7f0dd0c57c9c8d744665c1d4d
rainiken27 <motrifalgar@up.edu.ph>
2024-05-30 18:00:36 +0800

    placeholder for all titles page

---
+++ lib/all_titles.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+
+class AllTitlesScreen extends StatefulWidget {
+  const AllTitlesScreen({super.key});
+
+  @override
+  State<AllTitlesScreen> createState() => _AllTitlesScreenState();
+}
+
+class _AllTitlesScreenState extends State<AllTitlesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
