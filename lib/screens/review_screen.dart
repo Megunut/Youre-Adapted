b016d470c4f7f31543f1a59f5317ef1a98649283
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2024-05-31 04:48:19 +0800

    feat: Add bottom navigation bar to HomeScreen for easy navigation between top scores and all books

--- lib/screens/review_screen.dart
+++ lib/screens/review_screen.dart
@@ -1,9 +1,8 @@
 import 'package:book_review/widgets/book_card.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import '../widgets/book_card.dart';
 import 'submit_review_screen.dart';
-
+import 'package:intl/intl.dart';
 
 var db = FirebaseFirestore.instance;
 
@@ -12,11 +11,9 @@ class ReviewScreen extends StatefulWidget {
 
   final String userId;
 
-
   @override
   State<ReviewScreen> createState() => _ReviewScreenState();
 }
-
 
 class _ReviewScreenState extends State<ReviewScreen> {
   Key _refreshKey = UniqueKey();
@@ -28,130 +25,158 @@ class _ReviewScreenState extends State<ReviewScreen> {
     });
   }
 
-
   Future<DocumentSnapshot> getInfo() async {
     return FirebaseFirestore.instance.collection("book").doc(widget.userId).get();
   }
 
-
   @override
   Widget build(BuildContext context) {
-    
-
     return Scaffold(
-      
       appBar: AppBar(
         title: Text('Submit a Review'), // App bar title
       ),
-      body: SingleChildScrollView(
-        child: FutureBuilder(
-          future: getInfo(),
-          builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SectionHeader(title: "Book"),
-                  BookCard(
-                    title: snapshot.data!['bookTitle'],
-                    subtitle: snapshot.data!['author'],
-                    imageUrl: snapshot.data!['bookImageURL'],
-                    description: snapshot.data!['summary'],
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => SubmitReviewScreen(reviewType: "Book", bookID: snapshot.data!.id, ),
-                        ),
-                      );
-                    },
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            padding: const EdgeInsets.only(bottom: 80.0), // Add padding to avoid being overlapped by the button
+            child: FutureBuilder(
+              future: getInfo(),
+              builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SectionHeader(title: "Book"),
+                      BookCard(
+                        title: snapshot.data!['bookTitle'],
+                        subtitle: snapshot.data!['author'],
+                        imageUrl: snapshot.data!['bookImageURL'],
+                        description: snapshot.data!['summary'],
+                        onPressed: () {},
+                      ),
+                      SectionHeader(title: "Movie"),
+                      BookCard(
+                        title: snapshot.data!['adaptationTitle'].toString(),
+                        subtitle: snapshot.data!['author'],
+                        imageUrl: snapshot.data!['adaptationImageURL'],
+                        description: snapshot.data!['summary'],
+                        onPressed: () {},
+                      ),
+                      SectionHeader(title: "User Reviews"),
+                      _buildReviewList(snapshot.data!.id),
+                    ],
+                  );
+                } else if (snapshot.connectionState == ConnectionState.none) {
+                  return Text("No data");
+                }
+                return Center(child: CircularProgressIndicator());
+              },
+            ),
+          ),
+          Positioned(
+            bottom: 16.0,
+            left: 50.0, // Adjusted horizontal padding to make the button smaller
+            right: 50.0,
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => SubmitReviewScreen(bookID: widget.userId),
                   ),
-                  SectionHeader(title: "Movie"),
-                  BookCard(
-                    title: snapshot.data!['adaptationTitle'].toString(),
-                    subtitle: snapshot.data!['author'],
-                    imageUrl: snapshot.data!['adaptationImageURL'],
-                    description: snapshot.data!['summary'],
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => SubmitReviewScreen(reviewType: "Book", bookID: snapshot.data!.id, ),
-                        ),
-                      );
-                    },
-                  ),
-                ],
-              );
-              }else if (snapshot.connectionState == ConnectionState.none) {
-                return Text("No data");
-              }
-              return CircularProgressIndicator();
-            }
-        )
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor: Colors.pink, // text color
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18.0),
+                ),
+              ),
+              child: Text('Submit a Review'),
+            ),
+          ),
+        ],
       ),
     );
   }
 
+  Widget _buildReviewList(String bookID) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: db.collection('rating').where('bookID', isEqualTo: bookID).snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Center(child: CircularProgressIndicator());
+        }
+
+        var reviews = snapshot.data?.docs ?? [];
+        return ListView.builder(
+          shrinkWrap: true,
+          physics: NeverScrollableScrollPhysics(),
+          itemCount: reviews.length,
+          itemBuilder: (context, index) {
+            var review = reviews[index];
+            var timestamp = review['timestamp'] as Timestamp;
+            var date = timestamp.toDate();
+            var formattedDate = DateFormat('yyyy-MM-dd – kk:mm').format(date);
+
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                elevation: 3,
+                child: ListTile(
+                  leading: CircleAvatar(
+                    child: Icon(Icons.person),
+                  ),
+                  title: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            'Source Material Score: ${review['sourceMaterialScore']}',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          Icon(Icons.star, color: Colors.orange, size: 16),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            'Adaptation Score: ${review['adaptationScore']}',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          Icon(Icons.star, color: Colors.orange, size: 16),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            'Likeness Score: ${review['similarityScore']}',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          Icon(Icons.star, color: Colors.orange, size: 16),
+                        ],
+                      ),
+                      Text(
+                        'Submitted on: $formattedDate',
+                        style: TextStyle(color: Colors.grey, fontSize: 12),
+                      ),
+                    ],
+                  ),
+                  subtitle: Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: Text(review['comment']),
+                  ),
+                ),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
 }
-
-
-// class ReviewScreen extends StatelessWidget {
-//   final String bookTitle = "The Great Gatsby";
-//   final String bookSubtitle = "F. Scott Fitzgerald";
-//   final String bookImageUrl = "https://example.com/gatsby.jpg";
-//   final String bookDescription = "Midwesterner Nick Carraway is lured into the lavish world of his neighbor, Jay Gatsby...";
-
-//   final String movieTitle = "The Great Gatsby";
-//   final String movieSubtitle = "Directed by Baz Luhrmann";
-//   final String movieImageUrl = "https://example.com/gatsby_movie.jpg";
-//   final String movieDescription = "A Midwesterner becomes fascinated with his nouveau riche neighbor, who obsesses over his lost love.";
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final docRef = db.collection("cities").doc("SF");
-//       docRef.get().then(
-//         (DocumentSnapshot doc) {
-//           final data = doc.data() as Map<String, dynamic>;
-//           // ...
-//         },
-//         onError: (e) => print("Error getting document: $e"),
-//       );
-
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Submit a Review'),
-//       ),
-//       body: SingleChildScrollView(
-//         child: Column(
-//           crossAxisAlignment: CrossAxisAlignment.start,
-//           children: [
-//             SectionHeader(title: "Book"),
-//             BookCard(
-//               title: bookTitle,
-//               subtitle: bookSubtitle,
-//               imageUrl: bookImageUrl,
-//               description: bookDescription,
-//               onPressed: () {
-//                 // Handle submit review for book
-//               },
-//             ),
-//             SectionHeader(title: "Movie"),
-//             BookCard(
-//               title: movieTitle,
-//               subtitle: movieSubtitle,
-//               imageUrl: movieImageUrl,
-//               description: movieDescription,
-//               onPressed: () {
-//                 // Handle submit review for movie
-//               },
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
 
 class SectionHeader extends StatelessWidget {
   final String title;
@@ -169,6 +194,7 @@ class SectionHeader extends StatelessWidget {
     );
   }
 }
+
 
 // class BookCard extends StatelessWidget {
 //   final String title;
