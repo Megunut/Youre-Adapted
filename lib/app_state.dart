416af0815756d892c3eb6255ae2e37fe5eab3a19
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2024-05-31 06:50:11 +0800

    Refactor ApplicationState to initialize Firebase and set up authentication state listeners

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -5,28 +5,37 @@ import 'package:flutter/material.dart';
 
 import 'firebase_options.dart';
 
+// ApplicationState class that extends ChangeNotifier to manage authentication state
 class ApplicationState extends ChangeNotifier {
   ApplicationState() {
+    // Initialize Firebase and set up listeners on creation of the ApplicationState object
     init();
   }
 
-  bool _loggedIn = false;
-  bool get loggedIn => _loggedIn;
+  bool _loggedIn = false; // Private variable to track logged-in state
+  bool get loggedIn => _loggedIn; // Getter for the logged-in state
 
+  // Method to initialize Firebase and set up authentication state listeners
   Future<void> init() async {
+    // Initialize Firebase with options specific to the current platform
     await Firebase.initializeApp(
         options: DefaultFirebaseOptions.currentPlatform);
 
+    // Configure FirebaseUIAuth to use EmailAuthProvider for authentication
     FirebaseUIAuth.configureProviders([
       EmailAuthProvider(),
     ]);
 
+    // Listen to changes in the authentication state
     FirebaseAuth.instance.userChanges().listen((user) {
       if (user != null) {
+        // User is logged in
         _loggedIn = true;
       } else {
+        // User is logged out
         _loggedIn = false;
       }
+      // Notify listeners of changes to the logged-in state
       notifyListeners();
     });
   }
